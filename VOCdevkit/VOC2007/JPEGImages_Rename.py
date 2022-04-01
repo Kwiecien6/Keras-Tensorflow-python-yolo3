9c820c05586a2667781674995e5cbb46560d2d2d
Kwieceien6 <896836176@qq.com>
2022-04-01 09:29:11 +0800

    完成初始设置，成功运行初始程序，调用摄像头进行目标检测

---
+++ VOCdevkit/VOC2007/JPEGImages_Rename.py
@@ -0,0 +1,12 @@
+#批量重命名文件
+import os
+
+path = "F:/python_data/keras-yolo3/VOCdevkit/VOC2007/JPEGImages/" #放图片的文件夹
+index_no = 1
+for file in os.listdir(path):
+    imagepath = os.path.join(path, file)
+    newname = str(index_no) + '.jpg'
+    newimagename = os.path.join(path, newname)
+    os.rename(imagepath, newimagename)
+    print("以前的文件名和对应现在的序号", file + '---' + str(index_no))
+    index_no += 1
